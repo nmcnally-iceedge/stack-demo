7dd62e6b0fdb41518f8b6ea9be6f38fbe4547414
Nick McNally <159370054+nmcnally-iceedge@users.noreply.github.com>
2026-08-11 14:43:51 -0700

    DEMO-105: add QuoteFormatter used by QuoteController

--- src/QuoteController.java
+++ src/QuoteController.java
@@ -1,7 +1,8 @@
 public class QuoteController {
     private final QuoteService service = new QuoteService();
+    private final QuoteFormatter formatter = new QuoteFormatter();
 
     public String get() {
-        return service.current().text();
+        return formatter.format(service.current());
     }
 }
